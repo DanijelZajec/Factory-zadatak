e2b1ff15f9b78f302e9e645cfab7599b24d93f66
DanijelZajec <72875830+DanijelZajec@users.noreply.github.com>
2023-03-28 15:21:55 +0200

    Zadatak

---
+++ zadatak_danijel_zajec/script.js
@@ -0,0 +1,14 @@
+$('#slick1').slick({
+		rows: 2,
+		dots: false,
+		arrows: true,
+		infinite: true,
+		variableWidth: true,
+		speed: 400,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='arrow-gray-left.png'>",
+		nextArrow:"<img class='a-right control-c next slick-next' src='arrow-blue-right.png'>"
+});
+
+
